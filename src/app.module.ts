1564ae0d8735bd4d7ba2d8f855d0a55e3042b647
Giovani Boff <giovanisouza15@gmail.com>
2022-02-06 21:53:34 -0300

    removes bot module for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BotModule } from './bot/bot.module';
 
 @Module({
-  imports: [BotModule],
   controllers: [AppController],
   providers: [AppService],
 })
