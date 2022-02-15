4472e6137fd3f1ad04717578e918977401fa4ea8
Lucas Pacheco <lucaspdroz@gmail.com>
2022-02-14 21:42:55 -0300

    chore: adding functions [stop, pause and resume] #5-stop #6-skip #7-resume

--- src/modules/bot/bot.controller.ts
+++ src/modules/bot/bot.controller.ts
@@ -1,19 +1,19 @@
 import { SubCommand } from '@discord-nestjs/core';
-import { Controller} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { Content, Context, OnCommand } from 'discord-nestjs';
 import { CommandInteraction, Message } from 'discord.js';
 import { BotService } from './bot.service';
 
 @Controller()
-export class BotController{
-  
-    constructor(private botService: BotService){}
+export class BotController {
 
-    @OnCommand({ name: 'help'})
+    constructor(private botService: BotService) { }
+
+    @OnCommand({ name: 'help' || 'h' })
     async onHelp(
         @Content() content: string,
         @Context() [context]: [Message]
-    ): Promise<void>{
+    ): Promise<void> {
         await context.reply(`${content}`)
     }
 
@@ -21,7 +21,30 @@ export class BotController{
     async onPlay(
         @Content() content: string,
         @Context() [context]: [Message]
-    ): Promise<void>{
-        await this.botService.play(content,context)
+    ): Promise<void> {
+        await this.botService.play(content, context)
+    }
+
+    @OnCommand({ name: 'stop'})
+    async onStop(
+        @Content() content: string,
+        @Context() [context]: [Message]
+    ): Promise<void> {
+        await this.botService.stop(context)
+    }
+
+    @OnCommand({ name: 'pause'})
+    async onPause(
+        @Content() content: string,
+        @Context() [context]: [Message]
+    ): Promise<void> {
+        await this.botService.pause(context)
+    }
+    @OnCommand({ name: 'resume'})
+    async onResume(
+        @Content() content: string,
+        @Context() [context]: [Message]
+    ): Promise<void> {
+        await this.botService.resume(context)
     }
 }
